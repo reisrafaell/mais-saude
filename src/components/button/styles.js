3ea741d84cf743983f26aba2fbd21eb6573fdbd3
FernandoSegregio <segregio@gmail.com>
2023-01-21 20:09:30 -0300

    button

---
+++ src/components/button/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 80%;
+  height: 3.0625rem;
+  border: 1px solid #E53D00;
+  background-color: transparent;
+  border-radius: 0.375rem;
+  color: #E53D00;
+`;
