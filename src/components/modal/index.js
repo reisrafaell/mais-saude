6ae65d0c4f2ff165f050032eb2b19d589326114a
Rafael Reis <rafaeloliveira63@yahoo.com>
2023-01-22 00:58:34 -0300

    finalizando modal

---
+++ src/components/modal/index.js
@@ -0,0 +1,29 @@
+import * as S from "./modal.styles";
+import Text from "../text";
+import Image from "../image";
+import imageVacina from "../../assets/vacina.png";
+
+const Modal = ({ children, title }) => {
+  const closeModal = () => {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+  };
+  return (
+    <S.Section  id="modal">
+      <S.Container>
+        <S.ContainerHeader>
+          <S.CardIcon>
+            <Image img={imageVacina} width="3rem"></Image>
+          </S.CardIcon>
+          <Text fontSize="1.4rem" fontWeight="bold">
+            {title}
+          </Text>
+          <S.IconClose onClick={closeModal} />
+        </S.ContainerHeader>
+        {children}
+      </S.Container>
+    </S.Section>
+  );
+};
+
+export default Modal;
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -5,27 +5,40 @@ import Image from "../../components/image";
 import imagePerson from "../../assets/person.png";
 import imageLogo from "../../assets/logoBrand.png";
 import Card from "../../components/card";
+import Modal from "../../components/modal";
+import { useState } from "react";
 
 const Home = () => {
   const nav = useNavigate();
+  const [titleModal, setTitleModal] = useState();
 
+
+  const handleModal =(title)=>{
+    setTitleModal(title)
+    document.getElementById('modal').style.display = 'flex'
+
+
+  }
   return (
     <S.Container>
+      <Modal title={titleModal}>    
+      </Modal>
+      
       <S.ContainerHeader>
-          <Anchor
-            icon={true}
-            fontSize="1.5rem"
-            href="/access"
-            color={"#E53D00"}
-            margin="0.5rem 0"
-          >
-            Voltar
-          </Anchor>
-          <Image img={imageLogo} width="22rem" />
-        </S.ContainerHeader>
-      <S.ContainerCenter>        
+        <Anchor
+          icon={true}
+          fontSize="1.5rem"
+          href="/access"
+          color={"#E53D00"}
+          margin="0.5rem 0"
+        >
+          Voltar
+        </Anchor>
+        <Image img={imageLogo} width="22rem" />
+      </S.ContainerHeader>
+      <S.ContainerCenter>
         Home
-        <Card variant='card3'/>
+        <Card variant="card3" onClick={e=> handleModal('titulo')}/>
       </S.ContainerCenter>
     </S.Container>
   );
